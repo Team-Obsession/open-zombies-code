0e22560a3f14da171dd805ecc300c8fc2431a8d0
zachdebardlebon <zach.debardlebon@gmail.com>
2016-07-23 21:21:06 -0500

    Worked on it some
    Added some parameters

---
+++ Objects/MysteryBox.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MysteryBox : MonoBehaviour 
+{
+	//TODO: create file that has all of the guns/weapons
+	//TODO: create array or list of the weapons that we want in the mystery box
+
+	Weapon mysteryWeapon;
+	Weapon[] placeholder = new Weapon[20];
+	System.Random rng = new System.Random();
+
+	public Weapon roll()
+	{
+		int randomNumber = rng.Next(0,placeholder.Length);
+		mysteryWeapon = placeholder[randomNumber];
+		return mysteryWeapon;
+	}
+}
