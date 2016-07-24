77b8a2f80a8489e323b1cda0078d0323cc280bc9
zachdebardlebon <zach.debardlebon@gmail.com>
2016-07-24 17:37:43 -0500

    Gun and Weapon

--- Objects/Gun.cs
+++ Objects/Gun.cs
@@ -1,14 +1,138 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 
 public class Gun : Weapon 
 {
-	string weaponName, weaponClass;
+	public int magazineSize = 5, maxExcessAmmo;
+	private int currentExcessAmmo;
 
-	public Gun(string weaponName, string weaponClass)
+	private int bulletsInMag;
+
+	public int BulletsInMag
 	{
-		this.weaponName = weaponName;
-		this.weaponClass = weaponClass;
+		get {	return bulletsInMag;	}
+		protected set
+		{
+			if (value != bulletsInMag)
+			{
+				bulletsInMag = value;
+				OnBulletCountChange ();
+			}
+		}
 	}
 
+	public Gun()
+	{
+		bulletsInMag = magazineSize;
+	}
+		
+	/// <summary>
+	/// Reload this gun.
+	/// </summary>
+	public void Reload()
+	{
+		if(currentExcessAmmo > 0)
+		{
+			if(currentExcessAmmo >= magazineSize)
+			{
+				BulletsInMag = magazineSize;
+				currentExcessAmmo -= magazineSize;
+			}
+			else
+			{
+				BulletsInMag = currentExcessAmmo;
+				currentExcessAmmo = 0;
+			}
+		}
+		OnReload();
+	}
+
+	/// <summary>
+	/// Shoot up to the specified number of bullets.
+	/// Returns the actual number of bullets shot.
+	/// </summary>
+	/// <param name="numBullets">Number bullets.</param>
+	public int Shoot(int numBullets)
+	{
+		int bulletsShot = 0;
+		if (BulletsInMag > 0)
+		{
+			if(BulletsInMag >= numBullets)
+			{
+				BulletsInMag -= numBullets;
+				bulletsShot = numBullets;
+			}
+			else
+			{
+				BulletsInMag = 0;
+				bulletsShot = numBullets - BulletsInMag;
+			}
+		}
+		return bulletsShot;
+	}
+
+	protected Action cbBulletCountChange;
+	protected Action cbMagazineEmpty; 
+	protected Action cbReload;
+
+	private void OnBulletCountChange()
+	{
+		if(cbBulletCountChange != null)
+		{
+			cbBulletCountChange();
+		}
+	}
+
+	private void OnMagazineEmpty()
+	{
+		if(cbMagazineEmpty != null)
+		{
+			cbMagazineEmpty();
+		}
+
+	}		
+
+	private void OnReload()
+	{
+		if(cbReload != null)
+		{
+			cbReload();
+		}
+	}
+
+	public void RegisterBulletCountChange(Action callbackFunc)
+	{
+		cbBulletCountChange += callbackFunc;
+	}
+
+	public void UnregisterBulletCountChange(Action callbackFunc)
+	{
+		cbBulletCountChange -= callbackFunc;
+	}
+
+	public void RegisterMagazineEmpty(Action callbackFunc)
+	{
+		cbMagazineEmpty += callbackFunc;
+	}
+
+	public void UnregisterMagazineEmpty(Action callbackFunc)
+	{
+		cbMagazineEmpty -= callbackFunc;
+	}
+
+	public void RegisterReload(Action callbackFunc)
+	{
+		cbReload += callbackFunc;
+	}
+
+	public void UnregisterReload(Action callbackFunc)
+	{
+		cbReload -= callbackFunc;
+	}
+}
+	
+public enum GunClass
+{
+	LMG, Assault, SniperRifle
 }
--- Objects/Weapon.cs
+++ Objects/Weapon.cs
@@ -6,7 +6,10 @@ public class Weapon : ScriptableObject
 	public GameObject prefab;
 	public WeaponType weaponType;
 	public string weaponName;
-	public float movementScalar, fireRate, reloadSpeed;
+	public float movementScalar = 1f, fireRate = 480f, reloadSpeed = 1f;
+
+
+
 }
 
 public enum WeaponType
