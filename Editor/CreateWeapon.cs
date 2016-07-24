813d87a8de3bb33c0f4416115ad3b2e0bdcb08f6
cfalcione <caleb.falcione@gmail.com>
2016-07-24 18:22:23 -0500

    Fixed inability to create Weapon
    The name of the asset needed the relative path from the project folder.

--- Editor/CreateWeapon.cs
+++ Editor/CreateWeapon.cs
@@ -3,13 +3,13 @@ using UnityEditor;
 using System.Collections;
 
 public class CreateWeapon {
+
 	[MenuItem("Assets/Create/Weapon")]
-	
 	public static void CreateMyWeapon()
 	{
 		Weapon asset = ScriptableObject.CreateInstance<Weapon>();
 
-		AssetDatabase.CreateAsset (asset, "NewWeapon.asset");
+		AssetDatabase.CreateAsset (asset, "Assets/open-zombies-code/Weapons/NewWeapon.asset");
 		AssetDatabase.SaveAssets();
 
 		EditorUtility.FocusProjectWindow();
